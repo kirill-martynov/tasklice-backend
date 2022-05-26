d71d266590c4fdc2128f8565c8904f28da5404b5
Kirill Martynov <kirchik.martynov.995@gmail.com>
2022-05-27 00:58:14 +0300

    Feature, TKL-38, Add create task

---
+++ src/controllers/taskController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { Task } from "../models/taskModel";
+
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const task = await Task.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: { task },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
